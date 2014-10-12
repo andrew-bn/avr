988d6195621b6b2642c7fd34b60950d2e32fed0a
andrew-bn <mail.andrey.bulygin@gmail.com>
2014-10-12 22:16:48 +0300

    memory destroy, tests start

--- src/avros.asm
+++ src/avros.asm
@@ -8,7 +8,7 @@
 .CSEG
  jmp start
 .include "memory.inc"
-
+.include "memoryTests.inc"
  start:
 					; setup stack
 					ldi R16, low(RAMEND)
@@ -16,21 +16,35 @@
 					ldi R16, high(RAMEND)
 					out SPH, R16
 
+					call memoryTests_run
 					call memory_init
 
 					; call memory_allocate
-					ldi R16, low(10)
+					ldi R16, low(12)
 					push R16
-					ldi R16, high(10)
+					ldi R16, high(12)
 					push R16
-					call memory_allocate 
+
+					call memory_allocate
+					movw R19:R18, retH:retL
+					call memory_allocate
+					movw R21:R20, retH:retL
+					call memory_allocate
+					pop R16
+					pop R16
+					push R20
+					push R21
+					call memory_destroy;2
+					pop R16
+					pop R16
+					push R18
+					push R19
+					call memory_destroy;1
 					pop R16
 					pop R16
 					push retL
 					push retH
-					call memory_destroy
-
-					
+					call memory_destroy;3
 tmp:				rjmp tmp
 
 
--- src/doc/Memory.md
+++ src/doc/Memory.md
@@ -2,7 +2,7 @@
 Offcet|Name           |Size(bytes)|Description
 ----------------------------------------------
 0     |NextAddress    |2          | Next block address
-2     |PrevAddress    |2          | Prev block address. 0x0000 if this block is first, if Prev == Next - block is free
+2     |PrevAddress    |2          | Prev block address. if Prev == ramend, then block is free
 4     |Block          |N          |
 
 ----------------
