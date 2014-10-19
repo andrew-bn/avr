106ef219df77818bb01c214896a7b0c16a0cf733
andrew-bn <mail.andrey.bulygin@gmail.com>
2014-10-19 12:11:06 +0300

    kernel

---
+++ src/avr.asm
@@ -0,0 +1,29 @@
+/*
+ * avros.asm
+ *
+ *  Created: 08.10.2014 21:33:05
+ *   Author: AB
+ */
+ .listmac
+ .include "kernel.inc"
+
+.CSEG
+
+ start: rjmp start
+					
+
+					/*NEW list
+					movw AH:AL, RH:RL
+
+					NEW emptyTask
+					movw BH:BL,RH:RL
+
+					XCALLW AH:AL, list_addData, BH, BL
+				
+					DEL BH:BL
+					DEL AH:AL*/
+
+tmp2:				rjmp tmp2
+
+
+
